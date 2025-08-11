6490333a0d2a0996ed722eadafba2e26fcbdf08b
mostapha khalid <mustafaelkhokhy@gamil.com>
2025-08-11 11:48:33 +0300

    api/classes - permission -

--- app/Http/Controllers/Api/ClassesController.php
+++ app/Http/Controllers/Api/ClassesController.php
@@ -25,8 +25,6 @@ class ClassesController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-
-    
     public function store(Request $request)
     {
         //
